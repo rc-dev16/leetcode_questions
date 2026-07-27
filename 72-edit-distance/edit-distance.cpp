21990a0302e4e53de60caf87f18f7f80ca056427
Rohan Chawda <rohanc1604@gmail.com>
2026-07-28 00:49:19 +0530

    Time: 7 ms (60.71%) | Memory: 13.1 MB (72.80%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
+    int minDistance1(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
 
@@ -31,5 +31,36 @@ public:
         int replace = 1 + solve(word1, word2, m-1, n-1, dp);
 
         return dp[m][n] = min({insert, del, replace});
+    }
+
+     int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++ ){
+
+                if(word1[i-1] == word2[j-1]){
+                   dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int insert = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = min({insert, del, replace});
+                }
+            }
+        } 
+
+        return dp[m][n];       
+                   
     }
 };
