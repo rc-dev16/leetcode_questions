a6d6355a3561759c05aa73461787d5a5972ba5af
Rohan Chawda <rohanc1604@gmail.com>
2026-08-02 13:37:07 +0530

    Time: 13 ms (22.16%) | Memory: 19.6 MB (27.79%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         int total = accumulate(piles.begin(), piles.end(), 0);
+        int n = piles.size();
 
-        int alice = solve(0, piles.size() - 1, piles);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int alice = solve(0, piles.size() - 1, piles, dp);
 
         int bob = total - alice;
 
-        if(alice > bob) return alice;
+        if(alice > bob) return true;
 
-        return bob;
+        return false;
         
     }
 
-    int solve(int i , int j, vector<int>& piles){
-        if(i < j) return 0;
+    int solve(int i , int j, vector<int>& piles,  vector<vector<int>> &dp){
+        if(i > j) return 0;
         
         if(i == j) return piles[i];
 
-        int take_i = piles[i] + min(solve(i+2, j, piles), solve(i+1, j-1, piles));
-        int take_j = piles[j] + min(solve(i, j-2, piles), solve(i+1, j-1, piles));
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
 
-        return max(take_i, take_j);
+        int take_i = piles[i] + min(solve(i+2, j, piles, dp), solve(i+1, j-1, piles, dp));
+        int take_j = piles[j] + min(solve(i, j-2, piles, dp), solve(i+1, j-1, piles, dp));
+
+        
+
+        return dp[i][j] = max(take_i, take_j);
 
     }
 };
